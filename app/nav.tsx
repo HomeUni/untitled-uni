1221d232c97fc5bdeab5b07e310f9c39ef9662ae
Godfred Akpan <godfredakpan@gmail.com>
2023-10-20 17:05:34 +0100

    update auth

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,7 +1,6 @@
 import Navbar from './navbar';
-import { getServerSession } from 'next-auth/next';
 
 export default async function Nav() {
-  const session = await getServerSession();
+
   return <Navbar user={null} />;
 }
